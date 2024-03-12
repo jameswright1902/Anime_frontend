c310f1e5a300a825087359fc83013f208aa3b5c5
jwright1902 <jameswright1902@gmail.com>
2024-03-12 18:16:24 +0100

    added bootstrap

--- Anime_frontend/components/nav.jsx
+++ Anime_frontend/components/nav.jsx
@@ -6,22 +6,34 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
+        
         <Link className="navbar-brand" to="#">Home</Link>
+
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+        
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="#">Register</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="#">Sign in</Link>
+              <Link className="nav-link" to="#">Sign In</Link>
             </li>
             
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="#">recommendations</Link>
+              <Link className="nav-link active" aria-current="page" to="#">Schedules</Link>
             </li>
+
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="#">Characters</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="#">Anime</Link>
+            </li>
+
           </ul>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
