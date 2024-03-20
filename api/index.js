30be1835e81bb378e1d0e66e7ed4ad78a2178d60
jwright1902 <jameswright1902@gmail.com>
2024-03-20 20:57:59 +0100

    changed env and api index.js files for netlify

--- api/index.js
+++ api/index.js
@@ -3,11 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.SERVER || "https://localhost:5173/anime",
+    baseUrl: import.meta.env.VITE_SERVER || "https://localhost:5173/",
   }),
   endpoints: (builder) => ({
     getAnime: builder.query({
-      query: () => "/anime",
+      query: () => "/",
     }),
   }),
 });
